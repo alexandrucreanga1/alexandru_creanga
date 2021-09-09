0539f0233e9b124bf5988d76790f13b22e81df1d
alexandrucreanga1 <alexandru.creanga1@gmail.com>
2021-09-09 20:46:27 +0300

    commit - facut cateva modificari :)

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -9,9 +9,9 @@ package homework;
 import java.util.Scanner;
 
 public class Main {
-
+    //am adaugat ceva aditional;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter your weight(kg): ");
         double weight = scanner.nextDouble();
@@ -20,25 +20,23 @@ public class Main {
         double height = scanner.nextDouble();
 
 
+        double IMC = weight / Math.pow(height, 2);
 
 
-        double IMC = weight / Math.pow(height,2);
-
-
-        if ( weight <=0 || height <=0) {
+        if (weight <= 0 || height <= 0) {
             System.out.println("Please take into account, that numbers introduced for weight and/or height must be greater than zero (positive)");
-        } else if (IMC <=18.49) {
-            System.out.println("IMC este " + IMC +"; Status greutate: Subponderal");
-        } else if (IMC >=18.50 && IMC <=24.99) {
+        } else if (IMC <= 18.49) {
+            System.out.println("IMC este " + IMC + "; Status greutate: Subponderal");
+        } else if (IMC >= 18.50 && IMC <= 24.99) {
             System.out.println("IMC este " + IMC + "; Status greutate: Normala");
-        } else if (IMC >=25.00 && IMC <=29.99) {
-                System.out.println("IMC este " + IMC+"; Status greutate: Supraponderal");
-        }else if (IMC >=30.00 && IMC <=34.99) {
-            System.out.println("IMC este " + IMC+"; Status greutate: Obezitate (gradul I)");
-        }else if (IMC >=35.00 && IMC <=39.99) {
-            System.out.println("IMC este " + IMC+"; Status greutate: Obezitate (gradul II)");
-        }else {
-            System.out.println("IMC este " + IMC+"; Status greutate: Obezitate morbida");
+        } else if (IMC >= 25.00 && IMC <= 29.99) {
+            System.out.println("IMC este " + IMC + "; Status greutate: Supraponderal");
+        } else if (IMC >= 30.00 && IMC <= 34.99) {
+            System.out.println("IMC este " + IMC + "; Status greutate: Obezitate (gradul I)");
+        } else if (IMC >= 35.00 && IMC <= 39.99) {
+            System.out.println("IMC este " + IMC + "; Status greutate: Obezitate (gradul II)");
+        } else {
+            System.out.println("IMC este " + IMC + "; Status greutate: Obezitate morbida");
         }
         System.out.println("Thank you!");
     }
---
+++ src/homework/tema1/BMi.java
@@ -0,0 +1,13 @@
+package homework.tema1;
+
+public class BMi {
+    public static void main(String[] args) {
+
+        System.out.println("aici facem tema 1 he he");
+
+
+
+    }
+
+
+}
